be5c1e311accad3a6a3120bd62d4a68fb3c25f03
Artem Ufimtcev <artem@supernova.io>
2023-08-27 21:47:31 +0200

    Add isNonVirtualRoot support

--- src/functions.js
+++ src/functions.js
@@ -1,70 +1,73 @@
 /**
- * 
- * @param {string} text 
- * @param {string} indentationString 
- * 
+ *
+ * @param {string} text
+ * @param {string} indentationString
+ *
  * @returns {string}
  */
 function indentMultilineText(text, indentationString) {
-    return text.trim().split("\n").join("\n" + indentationString);
+  return text
+    .trim()
+    .split("\n")
+    .join("\n" + indentationString);
 }
 
 /**
- * 
- * @param {{name: string, isRoot: boolean, path: Array<string>}} tokenGroup 
- * 
+ *
+ * @param {{name: string, isRoot: boolean, path: Array<string>}} tokenGroup
+ *
  * @returns {Array<string>}
  */
 function createFullTokenGroupPath(tokenGroup) {
-    if (tokenGroup.isRoot) {
-        return []
-    } else {
-        return tokenGroup.path.concat(tokenGroup.name)
-    }
+  if (tokenGroup.isRoot || tokenGroup.isNonVirtualRoot) {
+    return [];
+  } else {
+    return tokenGroup.path.concat(tokenGroup.name);
+  }
 }
 
 /**
- * 
+ *
  * @param {Array<any>} lhs
  * @param {Array<any>} rhs
- * 
+ *
  * @returns {Array<any>}
  */
 function arrayConcat(lhs, rhs) {
-    return lhs.concat(rhs)
+  return lhs.concat(rhs);
 }
 
 /**
- * 
- * @param {Array<string>} array 
- * @param {string} separator 
+ *
+ * @param {Array<string>} array
+ * @param {string} separator
  */
 function arrayJoin(array, separator) {
-    return array.join(separator)
+  return array.join(separator);
 }
 
- function groupFontsByFamily(fonts) {
-    var groupBy = function(xs, key) {
-        return xs.reduce(function(rv, x) {
-          (rv[x[key].toLowerCase()] = rv[x[key].toLowerCase()] || []).push(x);
-          return rv;
-        }, {});
-      };
+function groupFontsByFamily(fonts) {
+  var groupBy = function (xs, key) {
+    return xs.reduce(function (rv, x) {
+      (rv[x[key].toLowerCase()] = rv[x[key].toLowerCase()] || []).push(x);
+      return rv;
+    }, {});
+  };
 
-    return groupBy(fonts, "family")
+  return groupBy(fonts, "family");
 }
 
 /**
- * 
+ *
  * @param {string} s
  */
 function isDigit(c) {
-    return c >= '0' && c <= '9';
+  return c >= "0" && c <= "9";
 }
 
-Pulsar.registerFunction("indentMultilineText", indentMultilineText)
-Pulsar.registerFunction("createFullTokenGroupPath", createFullTokenGroupPath)
-Pulsar.registerFunction("arrayConcat", arrayConcat)
-Pulsar.registerFunction("arrayJoin", arrayJoin)
-Pulsar.registerFunction("groupFontsByFamily", groupFontsByFamily)
-Pulsar.registerFunction("isDigit", isDigit)
+Pulsar.registerFunction("indentMultilineText", indentMultilineText);
+Pulsar.registerFunction("createFullTokenGroupPath", createFullTokenGroupPath);
+Pulsar.registerFunction("arrayConcat", arrayConcat);
+Pulsar.registerFunction("arrayJoin", arrayJoin);
+Pulsar.registerFunction("groupFontsByFamily", groupFontsByFamily);
+Pulsar.registerFunction("isDigit", isDigit);
